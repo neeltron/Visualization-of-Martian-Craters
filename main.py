f03e1ea4d90cb17d2ab2e8ee84e7e8dd2d9c28df
Neel Adwani <nprosofficial@gmail.com>
2021-06-30 11:11:05 +0000

    Added Visualization for the Number of Layers

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import pandas as pd
 import numpy as np
 import seaborn
 
+data = pd.read_csv('data/Mars Crater Dataset.csv')
+print(data)
 
+sub1 = data[(data["NUMBER_LAYERS"] >= 0)]
+sub2 = sub1.copy()
+
+plt = seaborn.countplot(x="NUMBER_LAYERS", data=sub2)
+fig = plt.get_figure()
+fig.savefig('a.png') 
